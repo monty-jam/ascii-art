58deeb36e30a1283d60055d40757ae93f1a8d5d0
Lev Popov <popovle1@fit.cvut.cz>
2023-12-21 11:29:18 +0100

    Added tests for loaders

--- src/main/scala/commands/loaders/fileloaders/FileRgbImageLoader.scala
+++ src/main/scala/commands/loaders/fileloaders/FileRgbImageLoader.scala
@@ -2,7 +2,7 @@ package commands.loaders.fileloaders
 
 import commands.loaders.RgbImageLoader
 import models.Images.RgbImage
-import models.PathFiles.FilePath
+import models.FilePaths.FilePath
 
 abstract class FileRgbImageLoader[T](override val filePath: FilePath) extends FileLoader[T, RgbImage](filePath) with RgbImageLoader {
 
--- src/test/scala/commands/loaders/JpgRgbImageLoaderTests.scala
+++ src/test/scala/commands/loaders/JpgRgbImageLoaderTests.scala
@@ -1,7 +1,24 @@
 package commands.loaders
 
+import commands.loaders.fileloaders.JpgRgbImageLoader
+import models.FilePaths.JpgFilePath
+import models.Images.RgbImage
 import org.scalatest.FunSuite
 
 class JpgRgbImageLoaderTests extends FunSuite {
+
+  def loaded(jpgFilePath: JpgFilePath): RgbImage = {
+    new JpgRgbImageLoader(jpgFilePath).load()
+  }
+
+  test("Load black and white jpg") {
+    val result = loaded(JpgFilePath("examples/blackWhite.jpg"))
+    //    assert(result.getPixel(0, 0).getValue == new Color(0x000000).getRGB)
+    //    assert(result.getPixel(0, 1).getValue == new Color(0xFFFFFF).getRGB)
+  }
+
+  test("Load non-existent jpg file") {
+    assertThrows[Exception](loaded(JpgFilePath("examples/nonExistentFile.jpg")))
+  }
 
 }
